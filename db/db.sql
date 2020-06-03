eec24600a1df21f0f641d11a7bb956e9120e19de
Stefano <nic17014@byui.edu>
2020-06-03 20:26:21 +0200

    sample db query for PostegreSQL

--- db/db.sql
+++ db/db.sql
@@ -1,1 +1,56 @@
-CREATE TABLE placeholder;
+CREATE TABLE Gamer (
+    GamerID SERIAL PRIMARY KEY NOT NULL,
+    GamerName VARCHAR(20) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
+);
+
+
+CREATE TABLE City (
+    CityID SERIAL PRIMARY KEY NOT NULL,
+    CityName VARCHAR(58) NOT NULL UNIQUE,
+    Latitude FLOAT NOT NULL,
+    Longitude FLOAT NOT NULL,
+    CityPopulation INT NOT NULL,
+	Extension FLOAT NOT NULL,
+	Elevation INT NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
+);
+
+CREATE TABLE Map (
+    MapID SERIAL PRIMARY KEY NOT NULL,
+    MapName VARCHAR(58) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
+);
+
+CREATE TABLE MapCity (
+    MapCityID SERIAL PRIMARY KEY NOT NULL,
+    CityID INT REFERENCES City(CityID) NOT NULL,
+    MapID INT REFERENCES Map(MapID) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    CONSTRAINT unique_map_city UNIQUE (CityID, MapID)
+);
+
+CREATE TABLE Game (
+    GameID SERIAL PRIMARY KEY NOT NULL,
+    MapID INT REFERENCES Map(MapID) NOT NULL,
+    GameDate DATE NOT NULL,
+    GameTime TIME NOT NULL,
+    Points INT NOT NULL,
+    GamerID INT REFERENCES Gamer(GamerID) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
+);
