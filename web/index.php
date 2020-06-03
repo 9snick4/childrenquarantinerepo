a4e7adaa9fa3d6dfe557101dcbf561db1947d883
Stefano <nic17014@byui.edu>
2020-06-03 20:19:24 +0200

    initial commit

--- web/index.php
+++ web/index.php
@@ -1,5 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-  header("location: /childquarantinerepo/index.php");
+  <head>
+    <?php include 'includes/head.php';?>
+  </head>
 
-?>
+  <body>
+
+    <!-- Navigation -->
+    <?php include 'includes/navigation.php';?>
+    
+    <!-- Header -->
+    <?php include 'includes/header.php';?>
+    
+    <!-- Main -->
+    <!--<?php include 'includes/main_index.php';?>-->
+    
+    <!-- Main -->
+    <?php include 'includes/footer.php';?>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
