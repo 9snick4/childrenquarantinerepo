d317588d8c11b356e77eb69da4a0add9dc90b15d
malagonius <47065208+malagonius@users.noreply.github.com>
2020-06-10 11:27:28 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,205 @@
-<?php
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="css.css">
+<div class="header">
+  <div class="search-container">
+    <div class="search-icon"></div>
+    <input type="text" name="search">
+  </div>
+</div>
 
-  header("location: /web/index.php");
 
-?>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="assets/img1.png" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+		    <h5>This is the first and landing page</h5>
+		    <p>Some content will go here to intro the user</p>
+  	  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img2.png" alt="Second slide">
+    	<div class="carousel-caption d-none d-md-block">
+		    <h5>We also Have some cool stuff</h5>
+		    <p>With some cool info about it</p>
+	  	</div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img3.png" alt="Third slide">
+    	<div class="carousel-caption d-none d-md-block">
+		    <h5>And don't forget about me</h5>
+		    <p>Since there is also a lot more to discover!</p>
+	  	</div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="container">
+  <div class="title">
+    <h1>Ciao BellA</h1>
+  </div>
+  <div class="subtitle">
+    <h2>Vendo Cocco BellA</h2>
+  </div>
+  <div class="footer-widgets"><div class="wrap">
+    <div class="widget-area footer-widgets-1 footer-widget-area">
+      <section id="text-10" class="widget widget_text">
+        <div class="widget-wrap">     
+          <div class="textwidget">
+            <div class="loghi">
+              <div class="brands-by-category">
+                <div class="category-name">Membro di:</div>
+                <div class="brands-container">
+                  <div class="logo-size-medium">
+                    <div id="foottip-597" class="tt-wrapper">
+                      <a href="http://www.nozio.com" target="_blank" rel="noopener noreferrer">
+                        <img width="48" height="48" src="https://www.palazzettomadonna.com/wp-content/uploads/2014/11/nozio_affiliated_hotel.png" class="size-medium" alt="" srcset="https://www.palazzettomadonna.com/wp-content/uploads/2014/11/nozio_affiliated_hotel.png 48w, https://www.palazzettomadonna.com/wp-content/uploads/2014/11/nozio_affiliated_hotel-32x32.png 32w" sizes="(max-width: 48px) 100vw, 48px">
+                      </a>
+                      <div class="tooltip-css">
+                        <div>
+                          <strong>Nozio</strong>
+                        </div>
+                        <div>Prenotare direttamente l'hotel, senza costi inutili di intermediazione: la scelta per un turismo economicamente sostenibile, conveniente, autentico e di qualità. Nozio ha selezionato questo hotel perché ti offre:<br><br> - un suo Sito Ufficiale di qualità dove prenotare a tariffe convenienti perché senza costi inutili di intermediazione;<br>- un buon rapporto qualità/prezzo confermato dalle opinioni degli ospiti iscritti alla Community Nozio.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="logo-size-medium">
+                    <div id="foottip-684" class="tt-wrapper">
+                      <a href="http://book2.nozio.com/v2/Campaign/CampaignDetails?channelId=08700e1a-7fef-4b3e-bdc1-c11006b9d0d9&amp;campaignId=07d6f836-252d-4f19-a3b6-cc966b7b49ad&amp;culture=it" target="_self" rel="noopener noreferrer"><img width="190" height="48" src="https://www.palazzettomadonna.com/wp-content/uploads/2014/11/noziotravel-it-w.png" class="size-medium" alt="">
+                      </a>
+                      <div class="tooltip-css">
+                        <div>
+                          <strong>Nozio Traveller</strong>
+                        </div>
+                        <div>Sei un Nozio Traveller? Accedi agli sconti extra riservati solo a te &gt;
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="brands-by-category">
+                <div class="category-name">Servizi</div>
+                <div class="brands-container">
+                  <div class="logo-size-medium">
+                    <div id="foottip-843" class="tt-wrapper">
+                      <img width="44" height="44" src="https://www.palazzettomadonna.com/wp-content/uploads/2015/04/wifi_free-bianco.png" class="size-medium" alt="" srcset="https://www.palazzettomadonna.com/wp-content/uploads/2015/04/wifi_free-bianco.png 44w, https://www.palazzettomadonna.com/wp-content/uploads/2015/04/wifi_free-bianco-32x32.png 32w" sizes="(max-width: 44px) 100vw, 44px">
+                      <div class="tooltip-css">
+                        <div>
+                          <strong>Wi-Fi Free</strong>
+                        </div>
+                        <div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="brands-by-category">
+                <div class="category-name">Social</div>
+                <div class="brands-container">
+                  <div class="logo-size-medium">
+                    <div id="foottip-2551" class="tt-wrapper">
+                      <a href="http://www.facebook.com/PalazzettoMadonnaVenice" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="42" src="https://www.palazzettomadonna.com/wp-content/uploads/2017/01/facebook.png" class="size-medium" alt="">
+                      </a>
+                      <div class="tooltip-css">
+                        <div>
+                          <strong>Facebook</strong>
+                        </div>
+                        <div></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="logo-size-medium">
+                    <div id="foottip-2552" class="tt-wrapper">
+                      <a href="http://www.twitter.com/#" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="42" src="https://www.palazzettomadonna.com/wp-content/uploads/2017/01/twitter.png" class="size-medium" alt="">
+                      </a>
+                      <div class="tooltip-css">
+                        <div>
+                          <strong>Twitter</strong>
+                        </div>
+                        <div></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="brands-by-category">
+                <div class="category-name">Certificazioni</div>
+                <div class="brands-container">
+                  <div class="logo-size-medium">
+                    <div id="foottip-2553" class="tt-wrapper">
+                      <a href="https://www.palazzettomadonna.com/wp-content/uploads/2017/01/ta2018.jpg" target="_blank" rel="gallery">
+                        <img width="85" height="64" src="https://www.palazzettomadonna.com/wp-content/uploads/2017/01/2018-it-85x64.png" class="size-medium" alt="" srcset="https://www.palazzettomadonna.com/wp-content/uploads/2017/01/2018-it-85x64.png 85w, https://www.palazzettomadonna.com/wp-content/uploads/2017/01/2018-it.png 93w" sizes="(max-width: 85px) 100vw, 85px">
+                      </a>
+                      <div class="tooltip-css"><div>
+                      <strong>Tripadvisor</strong>
+                    </div>
+                    <div>Certificato di Eccellenza 2018</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="brands-by-category">
+            <div class="category-name">Partner</div>
+            <div class="brands-container">
+              <div class="logo-size-medium">
+                <div id="foottip-2556" class="tt-wrapper">
+                  <a href="http://www.hotelsantamarina.it/" target="_blank" rel="noopener noreferrer">
+                    <img width="152" height="61" src="https://www.palazzettomadonna.com/wp-content/uploads/2017/01/logo-santa-marina-2.png" class="size-medium" alt="">
+                  </a>
+                  <div class="tooltip-css">
+                    <div>
+                      <strong>Hotel Santa Marina</strong>
+                    </div>
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <div class="anagrafica">
+        <p class="indirizzo">
+          <em>Palazzetto Madonna****L</em> di 
+          <a class="underline" href="#informazioni" rel="facebox">Naty.ma srl</a>
+          <br>San Polo 2902 – 30125 Venice, Italy | Tel. +39 041 5246781/5232335 Fax +39 041 2449108
+          <br>VAT 04120120276 – 
+          <a href="mailto:info@palazzettomadonna.com">info@palazzettomadonna.com</a>
+          <br>Palazzetto Madonna © 2000-
+          <script>document.write(new Date().getFullYear())</script>
+          <a href="http://www.nozio.biz/" target="_blank" rel="noopener noreferrer">Hotel Marketing by Nozio Business</a> | 
+          <a class="policy" href="http://www.nozio.biz/cr.htm" target="_blank" rel="nofollow noopener noreferrer">Cookies/Copyright/IP Policy</a> – 
+          <a class="policy" href="privacy.htm" target="_blank" rel="nofollow noopener noreferrer">Privacy</a>
+        </p>
+      </div>
+      <p>&nbsp;</p>
+      </div>
+    </div>
+  </section>
+</div>
+</div>
+</div>
+  <div class="contact-us"></div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
