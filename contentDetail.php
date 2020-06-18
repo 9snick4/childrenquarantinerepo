84282833b65b07052fe71b0d80f6c60ad57cc854
malagonius <47065208+malagonius@users.noreply.github.com>
2020-06-18 11:48:14 +0200

    Add files via upload

---
+++ contentDetail.php
@@ -0,0 +1,245 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="css.css">
+
+<div class="jumbotron jumbotron-fluid jumbotron-blue">
+  <div class="container">
+    <h1 class="display-4">Welcome to Category [CategoryName]</h1>
+    <p class="lead">Here you can find awesome Content about you special category!</p>
+  </div>
+</div>
+
+<div class="container">
+	<table class="table table-hover">
+	 	<thead>
+		    <tr>
+		      <th scope="col">#</th>
+		      <th scope="col">First</th>
+		      <th scope="col">Last</th>
+		      <th scope="col">Handle</th>
+		    </tr>
+	  	</thead>
+	  	<tbody>
+		    <tr>
+		      <th scope="row">1</th>
+		      <td>Mark</td>
+		      <td>Otto</td>
+		      <td>@mdo</td>
+		    </tr>
+		    <tr>
+		      <th scope="row">2</th>
+		      <td>Jacob</td>
+		      <td>Thornton</td>
+		      <td>@fat</td>
+		    </tr>
+		    <tr>
+		      <th scope="row">3</th>
+		      <td colspan="2">Larry the Bird</td>
+		      <td>@twitter</td>
+		    </tr>
+	  	</tbody>
+	</table>
+</div>
+
+<div class="contentImage">
+	<div class="container">
+		<div class="row">
+			<div class="card mb-3">
+			  	<div class="row no-gutters">
+			    	<div class="col-md-4">
+			      		<img src="..." class="card-img" alt="...">
+			    	</div>
+			    	<div class="col-md-8">
+			      		<div class="card-body">
+			        		<h5 class="card-title">Card title</h5>
+			        		<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		       			 	<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			      		</div>
+			    	</div>
+			  	</div>
+			</div>
+			<div class="card mb-3">
+			  	<div class="row no-gutters">
+			   	 	<div class="col-md-8">
+			      		<div class="card-body">
+			        		<h5 class="card-title">Card title</h5>
+		        			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		       			 	<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			      		</div>
+			    	</div>
+			    	<div class="col-md-4">
+				      <img src="..." class="card-img" alt="...">
+			    	</div>
+			  	</div>
+			</div>
+		</div>	
+	</div>
+</div>
+
+<div class="contentText">
+	<div class="accordion" id="accordionExample">
+	  <div class="card">
+	    <div class="card-header" id="headingOne">
+	      <h2 class="mb-0">
+	        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+	          Collapsible Group Item #1
+	        </button>
+	      </h2>
+	    </div>
+
+	    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+	      <div class="card-body">
+	        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+	      </div>
+	    </div>
+	  </div>
+	  <div class="card">
+	    <div class="card-header" id="headingTwo">
+	      <h2 class="mb-0">
+	        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+	          Collapsible Group Item #2
+	        </button>
+	      </h2>
+	    </div>
+	    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+	      <div class="card-body">
+	        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+	      </div>
+	    </div>
+	  </div>
+	  <div class="card">
+	    <div class="card-header" id="headingThree">
+	      <h2 class="mb-0">
+	        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+	          Collapsible Group Item #3
+	        </button>
+	      </h2>
+	    </div>
+	    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+	      <div class="card-body">
+	        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+	      </div>
+	    </div>
+	  </div>
+	</div>
+</div>
+
+<div class="contentVideo">
+	<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="false">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <div class="videoContainer">
+	      	<video width="400" controls>
+			  <source src="mov_bbb.mp4" type="video/mp4">
+			  <source src="mov_bbb.ogg" type="video/ogg">
+			  Your browser does not support HTML video.
+			</video>
+	      </div>
+	      <div class="carousel-caption d-none d-md-block">
+	        <h5>First slide label</h5>
+	        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+	      </div>
+	    </div>
+	    <div class="carousel-item">
+			<div class="videoContainer">
+		      	<video width="400" controls>
+				  <source src="mov_bbb.mp4" type="video/mp4">
+				  <source src="mov_bbb.ogg" type="video/ogg">
+				  Your browser does not support HTML video.
+				</video>
+	      </div>
+	      <div class="carousel-caption d-none d-md-block">
+	        <h5>Second slide label</h5>
+	        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+	      </div>
+	    </div>
+	    <div class="carousel-item">
+			<div class="videoContainer">
+		      	<video width="400" controls>
+				  <source src="mov_bbb.mp4" type="video/mp4">
+				  <source src="mov_bbb.ogg" type="video/ogg">
+				  Your browser does not support HTML video.
+				</video>
+	      	</div>
+	      <div class="carousel-caption d-none d-md-block">
+	        <h5>Third slide label</h5>
+	        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+	      </div>
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+</div>
+
+<div class="jumbotron jumbotron-fluid jumbotron-green">
+  <div class="container">
+    <h1 class="display-4">Not what where you looking for?</h1>
+    <p class="lead">Change the category right now!</p>
+  </div>
+</div>
+
+<div class="container">
+  <div class="title">
+    <h1>Ciao BellA</h1>
+  </div>
+  <div class="subtitle">
+    <h2>Vendo Cocco BellA</h2>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <div class="card" style="width: 18rem;">
+        <div class="imageContainer">
+          <img class="card-img-top" src="assets\card1.jpg" alt="Card image cap">
+           <a  href="#" class="learnMore">Learn More -></a>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="card" style="width: 18rem;">
+        <div class="imageContainer">
+            <img class="card-img-top" src="assets\card2.jpg" alt="Card image cap">
+            <a href="#" class="learnMore">Learn More -></a>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="card" style="width: 18rem;">
+        <div class="imageContainer">
+          <img class="card-img-top" src="assets\card3.jpg" alt="Card image cap">
+          <a href="#" class="learnMore">Learn More -></a>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
